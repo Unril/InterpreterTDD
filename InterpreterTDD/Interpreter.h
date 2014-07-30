cc131871a188740751ded850b0bf9ff257b85f53
unril <unril@live.ru>
2014-07-30 19:52:11 +0400

    Setup environment

---
+++ InterpreterTDD/Interpreter.h
@@ -0,0 +1,7 @@
+#pragma once;
+
+namespace Interpreter {
+
+using namespace std;
+
+}
---
+++ InterpreterTDD/InterpreterTests.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "Interpreter.h"
+
+namespace InterpreterTests {
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace Interpreter;
+using namespace std;
+
+TEST_CLASS(UnitTests) {
+public:
+
+    TEST_METHOD(TestMethod) {
+            
+    }
+
+};
+
+}
