55379a400b931c4746702f9d50cf36bc49a350a6
unril <unril@live.ru>
2014-07-31 16:05:18 +0400

    LexerTests Should_tokenize_single_plus_operator

--- InterpreterTDD/Interpreter.h
+++ InterpreterTDD/Interpreter.h
@@ -3,14 +3,25 @@
 
 namespace Interpreter {
 
-struct Token {};
+enum class Operator : wchar_t {
+    Plus = L'+',
+};
+
+typedef Operator Token;
+
+inline std::wstring ToString(const Token &token) {
+    return{ static_cast<wchar_t>(token) };
+}
 
 typedef std::vector<Token> Tokens;
 
 namespace Lexer {
 
-inline Tokens Tokenize(std::string expr) {
-    return{};
+inline Tokens Tokenize(std::wstring expr) {
+    if(expr.empty()) {
+        return{};
+    }
+    return{ static_cast<Operator>(expr[0]) };
 }
 
 } // namespace Lexer
--- InterpreterTDD/InterpreterTests.cpp
+++ InterpreterTDD/InterpreterTests.cpp
@@ -1,6 +1,11 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "Interpreter.h"
+#include <initializer_list>
+#include <algorithm>
+#include <string>
+
+#pragma warning(disable: 4505)
 
 namespace InterpreterTests {
 
@@ -8,11 +13,33 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace Interpreter;
 using namespace std;
 
+namespace AssertRange {
+
+template<class T, class ActualRange>
+static void AreEqual(initializer_list<T> expect, const ActualRange &actual) {
+    auto actualIter = begin(actual);
+    auto expectIter = begin(expect);
+
+    Assert::AreEqual(distance(expectIter, end(expect)), distance(actualIter, end(actual)), L"Size differs.");
+
+    for(; expectIter != end(expect) && actualIter != end(actual); ++expectIter, ++actualIter) {
+        auto message = L"Mismatch in position " + to_wstring(distance(begin(expect), expectIter));
+        Assert::AreEqual(*expectIter, *actualIter, message.c_str());
+    }
+}
+
+} // namespace AssertRange
+
 TEST_CLASS(LexerTests) {
 public:
     TEST_METHOD(Should_return_empty_token_list_when_put_empty_expression) {
-        Tokens tokens = Lexer::Tokenize("");
+        Tokens tokens = Lexer::Tokenize(L"");
         Assert::IsTrue(tokens.empty());
+    }
+
+    TEST_METHOD(Should_tokenize_single_plus_operator) {
+        Tokens tokens = Lexer::Tokenize(L"+");
+        AssertRange::AreEqual({ Operator::Plus }, tokens);
     }
 };
 
