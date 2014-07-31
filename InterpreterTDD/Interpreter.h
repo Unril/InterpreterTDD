ac674b47d4d579984670497e70593c692ec4ad1e
unril <unril@live.ru>
2014-07-31 17:21:55 +0400

    TokenTests

--- InterpreterTDD/Interpreter.h
+++ InterpreterTDD/Interpreter.h
@@ -1,5 +1,6 @@
 #pragma once;
 #include <vector>
+#include <assert.h>
 
 namespace Interpreter {
 
@@ -7,10 +8,67 @@ enum class Operator : wchar_t {
     Plus = L'+',
 };
 
-typedef Operator Token;
+inline std::wstring ToString(const Operator &op) {
+    return{ static_cast<wchar_t>(op) };
+}
+
+enum class TokenType {
+    Operator,
+    Number
+};
+
+inline std::wstring ToString(const TokenType &type) {
+    switch(type) {
+        case TokenType::Operator:
+            return L"Operator";
+        case TokenType::Number:
+            return L"Number";
+        default:
+            return L"Unknown token type";
+    }
+}
+
+class Token {
+public:
+    Token(Operator op) :m_type(TokenType::Operator), m_operator(op) {}
+
+    Token(double num) :m_type(TokenType::Number), m_number(num) {}
+
+    TokenType Type() const {
+        return m_type;
+    }
+
+    operator Operator() const {
+        if(m_type != TokenType::Operator) {
+            throw std::logic_error("Should be operator token.");
+        }
+        return m_operator;
+    }
+
+    operator double() const {
+        if(m_type != TokenType::Number) {
+            throw std::logic_error("Should be number token.");
+        }
+        return m_number;
+    }
+
+private:
+    TokenType m_type;
+    union {
+        Operator m_operator;
+        double m_number;
+    };
+};
 
 inline std::wstring ToString(const Token &token) {
-    return{ static_cast<wchar_t>(token) };
+    switch(token.Type()) {
+        case TokenType::Number:
+            return std::to_wstring(static_cast<double>(token));
+        case TokenType::Operator:
+            return ToString(static_cast<Operator>(token));
+        default:
+            "Unknown token.";
+    }
 }
 
 typedef std::vector<Token> Tokens;
--- InterpreterTDD/InterpreterTests.cpp
+++ InterpreterTDD/InterpreterTests.cpp
@@ -24,7 +24,7 @@ static void AreEqual(initializer_list<T> expect, const ActualRange &actual) {
 
     for(; expectIter != end(expect) && actualIter != end(actual); ++expectIter, ++actualIter) {
         auto message = L"Mismatch in position " + to_wstring(distance(begin(expect), expectIter));
-        Assert::AreEqual(*expectIter, *actualIter, message.c_str());
+        Assert::AreEqual<T>(*expectIter, *actualIter, message.c_str());
     }
 }
 
@@ -40,6 +40,34 @@ public:
     TEST_METHOD(Should_tokenize_single_plus_operator) {
         Tokens tokens = Lexer::Tokenize(L"+");
         AssertRange::AreEqual({ Operator::Plus }, tokens);
+    }
+
+    TEST_METHOD(Should_tokenize_single_digit) {
+        Tokens tokens = Lexer::Tokenize(L"1");
+        AssertRange::AreEqual({ 1.0 }, tokens);
+    }
+};
+
+TEST_CLASS(TokenTests) {
+public:
+    TEST_METHOD(Should_get_type_for_operator_token) {
+        Token opToken(Operator::Plus);
+        Assert::AreEqual(TokenType::Operator, opToken.Type());
+    }
+
+    TEST_METHOD(Should_get_type_for_number_token) {
+        Token numToken(1.2);
+        Assert::AreEqual(TokenType::Number, numToken.Type());
+    }
+
+    TEST_METHOD(Should_get_operator_code_from_operator_token) {
+        Token token(Operator::Plus);
+        Assert::AreEqual<Operator>(Operator::Plus, token);
+    }
+
+    TEST_METHOD(Should_get_number_value_from_number_token) {
+        Token token(1.23);
+        Assert::AreEqual<double>(1.23, token);
     }
 };
 
